efc135c01cdae7f44f4f28ffd65cfd07134b56ae
Tandeep Gill <gill.tandeep@gmail.com>
2022-02-11 15:53:42 +0000

    Layout created to hold Header and Footer.

---
+++ components/layout/layout.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+
+import Header from './header';
+import Footer from './footer';
+
+const Layout = (props) => {
+  return (
+    <Fragment>
+      <Header />
+      <main>{props.children}</main>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Layout;
