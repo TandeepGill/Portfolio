90f66f11ee9ffb4591113edb70cc80ce109e14a8
Tandeep Gill <gill.tandeep@gmail.com>
2022-02-16 12:18:40 +0000

    Inter font added and favicon updated.

--- pages/index.js
+++ pages/index.js
@@ -4,5 +4,16 @@ import Image from 'next/image';
 // Sectional Components Go Here
 
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  // return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+      <Head>
+        <title>Tandeep Gill SWE Portfolio</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
+        <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
     colors: {
       black: '#0B0C10',
       slate: '#1F2833',
