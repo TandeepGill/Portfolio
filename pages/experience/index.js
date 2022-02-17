859120940d682e50f8b19a0dd1c0e2b4564ad957
Tandeep Gill <gill.tandeep@gmail.com>
2022-02-17 17:12:56 +0000

    Placeholder index.js files added for all routes.

---
+++ pages/experience/index.js
@@ -0,0 +1,11 @@
+const Experience = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+      <div className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
+        <h1 className='text-3xl font-bold underline'>Experience Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
