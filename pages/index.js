67a1b5f1ec5edaa4cde77dfc0db46ec0136c7035
Tandeep Gill <gill.tandeep@gmail.com>
2022-03-01 14:09:10 -0800

    Home page created, mobile responsive.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
-import Image from 'next/image';
+
+const callToActionHandler = () => {
+  return window.open('mailto:gill.tandeep@gmail.com', '_blank');
+};
 
 const Home = () => {
   return (
@@ -7,8 +10,30 @@ const Home = () => {
       <Head>
         <title>Tandeep Gill - Full Stack SWE</title>
       </Head>
-      <div className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
-        <h1 className='text-3xl font-bold underline'>Home Page</h1>
+      <div>
+        <div className='items-left mb-32 flex w-full flex-1 flex-col justify-center px-12 lg:mr-48 lg:px-20'>
+          <h1 className='mb-4 text-lg text-white lg:mb-7 lg:text-xl'>
+            Hi, my name is
+          </h1>
+          <h2 className='mb-4 text-4xl font-bold lg:mb-7 lg:text-6xl'>
+            Tandeep Gill.
+          </h2>
+          <h3 className='text-teal mb-4 text-3xl font-bold lg:mb-7 lg:text-5xl'>{`I'm a Full Stack Software Engineer.`}</h3>
+          <p className='mb-4 max-w-2xl text-lg text-white lg:mb-7 lg:text-lg'>
+            {`The world is continuously evolving and technology is becoming an integral part in all aspects of
+            society, as there is a multiplying stream of challenges faced on a
+            daily basis by companies and consumers. It's my passion and love for
+            solving these challenges, which ultimately drove me to become a
+            software engineer.`}
+          </p>
+          <button
+            className='bg-aqua hover:bg-teal text-slate mt-4 w-44 items-center rounded-md border border-transparent px-6 py-3 text-base font-semibold hover:text-white lg:mt-7'
+            type='button'
+            onClick={callToActionHandler}
+          >
+            Get In Touch!
+          </button>
+        </div>
       </div>
     </div>
   );
